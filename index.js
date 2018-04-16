2d693ff256595c36331e94bbd7b547a797da75bc
Lorena <lorena.giddens@outlook.com>
2018-04-16 05:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
       cart.length = 0;
-      return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}
+      return `Your total cost is $${cost}, which will be charged to the card ${cardNumber}.`;
   }
 }
